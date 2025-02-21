cd9c5797541899344258b40b143ea1cf6a0dea20
Jihyung Lee <young@delightlabs.io>
2025-02-21 16:39:40 +0900

    chore: tidy up structure

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div data-layer="Pennywise - Desktop" className="PennywiseDesktop"
+           style={{width: 1440, height: 4483, position: 'relative', background: 'white'}}>
+        <div data-layer="Empowering Beauty, One Click at a Time." className="EmpoweringBeautyOneClickAtATime" style={{
+          width: 696.47,
+          height: 156.76,
+          left: 68,
+          top: 285,
+          position: 'absolute',
+          color: 'black',
+          fontSize: 63.78,
+          fontFamily: 'ITC Benguiat Std',
+          fontWeight: '700',
+          wordWrap: 'break-word'
+        }}>Empowering Beauty, One Click at a Time.
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
